6c0f9444ca77cd256a779f5c1acd25952fe35c11
mistergates <gates55434@gmail.com>
2021-03-12 01:02:24 -0600

    Fixed pathing for python venv.

--- package.bat
+++ package.bat
@@ -4,8 +4,8 @@ IF ERRORLEVEL 1 SET REPO=
 IF ERRORLEVEL 2 SET REPO=--repository testpypi
 
 @ECHO ON
-%~dp0\venv\bin\python -m pip install --upgrade build
-%~dp0\venv\bin\python -m pip install --upgrade twine
+%~dp0\venv\Scripts\python -m pip install --upgrade build
+%~dp0\venv\Scripts\python -m pip install --upgrade twine
 
-%~dp0\venv\bin\python -m build
-%~dp0\venv\bin\python -m twine upload %REPO% dist/*
+%~dp0\venv\Scripts\python -m build
+%~dp0\venv\Scripts\python -m twine upload %REPO% dist/*
